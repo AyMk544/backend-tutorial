51ee7f20b7379742e4dc20594f7af976e6d3f124
Ayush <ayushmukkanwar2004@gmail.com>
2024-05-23 18:14:50 +0530

    create api and handle errors

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({ limit: "16kb" })); // input data format is json with a set limit
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); // helps in understanding different types of url
+app.use(express.static("public")) // static files will be stored in public folder
+app.use(cookieParser())
+
+export { app }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import dotenv from "dotenv"
 import express from "express"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
-connectDB();
+const PORT = process.env.PORT;
+
+connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("ERROR!!: ", error);
+            throw error;
+        })
+
+        app.listen(PORT || 8000, () => {
+            console.log(`Server is running at port ${PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log("MONGODB connection failed: ", err);
+    });
 
 /*
 (async () => {
